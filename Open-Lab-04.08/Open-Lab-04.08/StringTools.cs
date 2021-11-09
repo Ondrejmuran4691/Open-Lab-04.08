c487db54c758ea7a40910c4b976fe5871f64e347
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-09 21:20:45 +0100

    returns 4 letters long string from string array

---
+++ Open-Lab-04.08/Open-Lab-04.08/StringTools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Open_Lab_04._08
+{
+    public class StringTools
+    {
+        public string[] IsFourLetters(string[] strings)
+        {
+            var done = new List <string>();
+          
+            foreach (string s in strings)
+            {
+                
+                if(s.Length == 4)
+                {
+                    done.Add(s);
+                }
+            }
+            return done.ToArray();
+
+        }
+    }
+}
